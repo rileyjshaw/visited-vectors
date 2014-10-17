a0c05abcc8f8cb1de6a7a5c33ab293ae90743fd6
Riley Shaw <rileyjshaw@gmail.com>
2014-10-16 18:37:22 -0700

    Finish initial working search tree version

--- reaction/reaction-256n-logn.js
+++ reaction/reaction-256n-logn.js
@@ -1,22 +1,27 @@
-// ;(function(window) {
+;(function(window) {
   var sharedState = {
+    // lets us check for a long period of activity and pop
+    // up a dummy element so that they don't get bored
+    startTime: null,
+    // lets us display their score in ms
     stepTime: null,
+    nextStep: null,
     currentChunk: [],
     // a stack for the step() function; simple recursive traversal of the
     // tree on the call stack isn't going to cut it since we need to wait
     // for user input to evaluate a branch's truthiness
     steps: [],
+    // not really needed since we can just check nextStep, but it's clearer
     paused: true,
     inCountdown: false,
-    // check for a false positive
-    isReal: true,
     // number of links tested per probe
     n: 16,
     DOM: {
-      container: document.querySelector('#container'),
+      container: document.getElementById('container'),
+      blocker: document.getElementById('blocker'),
       anchors: null,
-      ms: document.querySelector('#ms'),
-      instructions: document.querySelector('#instructions')
+      ms: document.getElementById('ms'),
+      instructions: document.getElementById('instructions')
     },
     urls: [],
     visitedUrls: [],
@@ -41,7 +46,7 @@
     sharedState.DOM.anchors = Array.prototype.slice.call(container.children, 1);
   }
 
-  function nextChunk () {
+  function getNextChunk () {
     // pop the sites from sharedState.urls
     return sharedState.urls.splice(-sharedState.n);
   }
@@ -51,60 +56,83 @@
     for (var i = 0; i < n; i++) {
       href = sites[i];
       for (var j = i, _len = n * 256; j < _len; j += n) {
-        if (href || sharedState.DOM.anchors[j].href !== '') sharedState.DOM.anchors[j].href = href;
+        if (href || sharedState.DOM.anchors[j].href !== '') {
+          sharedState.DOM.anchors[j].href = href;
+        }
       }
     }
-    //console.log('@' + new Date().getTime() + ' finished populate');
-
-    // turns out we don't need this in recent Chrome,
-    // leaving it on standby until I've tested other browsers
-    // repaint(sharedState.DOM.container);
   }
 
-  function step (duration) {
-    var timeUntilNext, currentLength = sharedState.currentChunk.length;
-    duration = duration || 0;
-    sharedState.stepTime = new Date().getTime();
-    //console.log('@' + sharedState.stepTime + ' started next step');
+  function step () {
+    var nextChunk, currentLength = sharedState.currentChunk.length;
 
-    if (duration > 20000) {
-      // todo add an isReal on sharedState so that nothing gets pushed to the visited array
-      sharedState.nextStep = false;
+    // if we've gone 15s without a hit, simulate one so they don't get bored
+    if (new Date().getTime() - sharedState.startTime > 15000) {
+      // handleKeydown will notice that nextStep is null and not run
+      // splitToStack, avoiding a false positive
+      sharedState.nextStep = null;
       sharedState.DOM.container.className = 'dummy';
     } else {
-      // if there's nothing on the stack, grab the next chunk
-      sharedState.currentChunk = sharedState.steps.length ?
+      nextChunk = sharedState.steps.length ?
         sharedState.steps.pop() :
-        nextChunk();
-      if (currentLength !== sharedState.n && currentLength === sharedState.currentChunk.length) {
-        splitToStack(sharedState.currentChunk);
+        getNextChunk();
+      // wait until after render to update sharedState.currentChunk = nextChunk
+
+      // if we passed on the first branch, the other branch must have a visited
+      // link in it so we can skip that check & dive straight into it
+      if (currentLength !== sharedState.n && currentLength === nextChunk.length) {
+        splitToStack(nextChunk);
         step();
       } else {
-        populate(sharedState.currentChunk);
-        // run it again in [800, 1000)ms
-        timeUntilNext = Math.floor((Math.random() * 200) + 800);
-        sharedState.nextStep = window.setTimeout(function () {
-          step(duration + timeUntilNext);
-        }, timeUntilNext);
+        // this is going to queue a big render...
+        populate(nextChunk);
+        // ...so we async this so that state only changes after the render...
+        window.setImmediate(function () {
+          // ...then push it to the back of the task queue again after render
+          // to let keyEvents resolve first.
+          window.setImmediate(function () {
+            // the render is done. hurrah.
+            sharedState.stepTime = new Date().getTime();
+
+            // we're safe to change state now, since handleKeydown will
+            // have already passed the old state to splitToStack
+            sharedState.currentChunk = nextChunk;
+
+            // if we're not paused, step again in [1000, 1200)ms + render time.
+            // longer than linear version to (hopefully) avoid someone hitting
+            // spacebar during render, which causes awkwardness (slow score
+            // or "bad" when a decent score is expected)
+            if (!sharedState.paused) {
+              sharedState.nextStep = window.setTimeout(function () {
+                step();
+              }, Math.floor((Math.random() * 200) + 1000));
+            }
+          });
+        });
       }
     }
   }
 
   function start () {
     function countdown (stage) {
-      if (stage) {
-        sharedState.DOM.ms.textContent = stage;
+      if (stage >= 0) {
+        sharedState.DOM.ms.textContent = stage ? stage : 'GO';
         window.setTimeout(function () {
           countdown(stage - 1);
         }, 500);
       } else {
-        sharedState.DOM.ms.textContent = 'GO';
-        window.setTimeout(function () {
+        sharedState.DOM.ms.textContent = '';
+        repaint(sharedState.DOM.blocker); // make sure...
+
+        // let "go" render
+        window.setImmediate(function () {
+          // also set stepTime in case they click before the first render
+          sharedState.stepTime = sharedState.startTime = new Date().getTime();
           sharedState.DOM.container.className = '';
           sharedState.DOM.ms.textContent = '';
           sharedState.inCountdown = false;
           step();
-        }, 500);
+        });
       }
     }
     sharedState.inCountdown = true;
@@ -126,11 +154,11 @@
 
   function handleKeydown (e) {
     var key = e.key || e.keyCode;
-    console.log('@' + new Date().getTime() + ' pressed spacebar');
+
     if (!sharedState.inCountdown && key === 32) {
       if (sharedState.paused) {
         sharedState.paused = false;
-        sharedState.DOM.instructions.textContent = "when red";
+        sharedState.DOM.instructions.textContent = 'when red';
         start();
       } else {
         if (sharedState.nextStep) {
@@ -138,9 +166,10 @@
           sharedState.DOM.container.className = 'non-blocking';
           splitToStack(sharedState.currentChunk);
         }
-        sharedState.DOM.instructions.textContent = "to retry";
+        sharedState.DOM.instructions.textContent = 'to retry';
         sharedState.paused = true;
-        sharedState.DOM.ms.innerText = new Date().getTime() - sharedState.stepTime + 'ms';
+        sharedState.DOM.ms.innerText =
+          new Date().getTime() - sharedState.stepTime + 'ms';
       }
     } else if (key === 65) {
       // prevent meta + a for select all
@@ -168,8 +197,8 @@
 
       if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(userAgent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(userAgent.substr(0,4))) {
         // we're on a mobile device
-        document.querySelector('#verb').textContent = 'Tap';
-        document.querySelector('#noun').textContent = 'screen';
+        document.getElementById('verb').textContent = 'Tap';
+        document.getElementById('noun').textContent = 'screen';
         window.addEventListener('touchstart', function () {
           handleKeydown({key: 32}); // lol
         }, false);
@@ -180,4 +209,4 @@
   };
 
   request.send();
-// })(this);
+})(this);
